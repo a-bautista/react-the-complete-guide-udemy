f7eb9211fa8797b4f9212e94018c3f0f0d5d7405
alexbr9007 <alex.bautista.ramos.90@gmail.com>
2019-01-20 01:26:22 -0700

    Added the ErrorBoundaries files.

--- Section-5/same-project-css-enabled/src/App.js
+++ Section-5/same-project-css-enabled/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+
 
 class App extends Component {
   state = {
@@ -52,12 +54,14 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map( ( person, index ) => {
-            return <Person
+            return <ErrorBoundary   key={person.id}>
+            <Person
               click={() => this.deletePersonHandler( index )}
               name={person.name}
               age={person.age}
-              key={person.id}
-              changed={( event ) => this.nameChangedHandler( event, person.id )} />
+              changed={( event ) => this.nameChangedHandler( event, person.id )} 
+            />
+            </ErrorBoundary>
           } )}
         </div>
       );

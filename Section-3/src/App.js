fb9d2cecc1d53e7d38fac9a715db3f1a36ae8ca0
Alex BR <alex.bautista.ramos.90@gmail.com>
2019-09-02 15:22:36 -0600

    Added some review notes.

--- Section-3/src/App.js
+++ Section-3/src/App.js
@@ -4,35 +4,58 @@ import Person from "./Person/Person";
 
 class App extends Component {
   /*  state is an object property from the Component class that contains the people property. 
+
       state name convention cannot be modified.
+
+      React Hooks is a collection of functions exposed by React where you can use them in functional components. 
+      React Hooks usually start with the keyword use.
+      React Hooks are available in React 16.8.
+
+      Stateless vs Stateful: In stateless, there are no internal state management (usually they receive data and they
+        display it).
+      In stateful, we have containers that have the state of the components. 
+      It is a good practice to have more stateless component than stateful components. 
   */
 
   state = {
     people: [
       { name: "Alex", age: 29 },
       { name: "Liz", age: 20 },
-      { name: "Stephanie", age: 30 }
+      { name: "Stephanie", age: 30 },
+      { name: "Jeremiah", age: 45 }
     ]
   };
 
   switchNameHandler = newName => {
-    //console.log("This was clicked!");
-    // avoid using the following syntax for changing a state -> this.state.people[0].name = 'Alejandro'
+    /*
+      The purpose of this handler is to change the state of the property people to the new values.
+      newName is used to change the state of people to the one that is given with the bind() method or
+      with the new name that is inside of the onClick(). 
+    */
+
+    // The following is invalid because you cannot mutate the state of a variable like -> this.state.people[0].name = 'Alejandro'
     this.setState({
       people: [
         { name: newName, age: 22 },
         { name: "Liz", age: 24 },
-        { name: "Stephanie", age: 32 }
+        { name: "Stephanie", age: 32 },
+        { name: "Jeremiah", age: 40 }
       ]
     });
   };
 
   nameChangeHandler = event => {
+    /*
+      The purpose of this handler is to listen to an event (typing in the input box) and then replace the given 
+      text with the new one. 
+    */
+
     this.setState({
       people: [
-        { name: "Alejandro Bautista Ramos", age: 29 },
-        { name: event.target.value, age: 24 },
-        { name: "Stephanie", age: 32 }
+        { name: "Alex", age: 29 },
+        { name: event.target.value, age: 20 },
+        { name: "Stephanie", age: 30 },
+        { name: "Jeremiah", age: 45 }
       ]
     });
   };
@@ -41,7 +64,7 @@ class App extends Component {
     const style = {
       backgroundColor: "white",
       font: "inherit",
-      border: "1px solid blue",
+      border: "3px solid blue",
       padding: "8px",
       cursor: "pointer"
     };
@@ -56,6 +79,8 @@ class App extends Component {
           The this keyword refers to the class App and we specified the property that we defined.
 
           Remember that the code from below is JSX (JavaScript compiled)
+
+          In arrow functions, the return statement is not typed because they already contain that statement. 
        */
 
       <div className="App">
@@ -67,24 +92,32 @@ class App extends Component {
           >
             Click on me to switch my name
           </button>
-          <p>I am a child 1</p>
-        </Person>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>
-          <p>I am a child 2</p>
-        </Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}>
-          <p>I am a child 3</p>
+          <p>I am the child 1</p>
         </Person>
         <Person
-          //The syntax from below is more efficient than the one from above.
-          click={this.switchNameHandler.bind(this, "Alejandro")}
+          name={this.state.people[1].name}
+          age={this.state.people[1].age}
           changed={this.nameChangeHandler}
         >
-          Click here!!!
+          <p>I am the child 2</p>
+          <p>
+            Type in the input box any text you want to change the original name.
+          </p>
         </Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}>
+          <p>I am the child 3</p>
+        </Person>
+        <p>Click in any part of the text below.</p>
+        <Person
+          name={this.state.people[3].name}
+          age={this.state.people[3].age}
+          // The syntax from below is more efficient than -> this.switchNameHandler("Alejandro Bautista")}, however,
+          // onClick is used for buttons whereas click is used for any part in the html file.
+          click={this.switchNameHandler.bind(this, "Alejandro")}
+        ></Person>
       </div>
       // html element, css, element to render
-      // The commented section describes how React renders the html elements and it's the same as the code from above.
+      // The commented section down below describes how React renders the html elements and it's the same as the code from above.
       // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello Alex!'))
     );
   }
--- Section-3/src/Person/Person.js
+++ Section-3/src/Person/Person.js
@@ -1,9 +1,39 @@
 import React from "react";
 import "./Person.css";
 
-/*  props is a convention parameter, you can type in any name you want
+/*  
+    props is a convention parameter, you can type in any name you want
     props.children displays the the content that is between <Person></Person>
-    Below is a functional component
+    Below you have a functional component where props.click is the property that allows to click on any part of the
+    <p> tag to change the state of the class and props.changed is the property that changes the name to any given
+    text that is typed in the input text field. 
+
+    You define what you want to change in the nameChangeHandler, then you define in the Person.js component what
+    exactly you will change and lastly you define how to call this new event with the changed property. 
+
+    nameChangeHandler = event => {
+        this.setState({
+            people: [
+                { name: "Alex", age: 29 },
+                { name: event.target.value, age: 20 },
+                { name: "Stephanie", age: 30 },
+                { name: "Jeremiah", age: 45 }
+                ]
+            });
+        };
+
+    <div>
+        <Person
+            name={this.state.people[1].name}
+            age={this.state.people[1].age}
+            changed={this.nameChangeHandler}
+            >
+        </Person>
+    </div>
+
+    changed is a property already defined in the Person.js component
+
+    <input type="text" onChange={props.changed} />
 */
 
 const person = props => {
@@ -13,7 +43,7 @@ const person = props => {
         I am a {props.name} and I am {props.age} years old.
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} />
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };

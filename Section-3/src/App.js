8b5169b675d636343f93608580db0e52c0e12e54
Alex BR <alex.bautista.ramos.90@gmail.com>
2019-09-01 23:50:57 -0600

    Took notes about the lecture.

--- Section-3/src/App.js
+++ Section-3/src/App.js
@@ -1,72 +1,91 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
-  state = {
-      people: [{name: 'Alex', age: 28},
-               {name: 'Liz', age: 29},
-               {name: 'Stephanie', age: 30}
-              ]
-          }
+  /*  state is an object property from the Component class that contains the people property. 
+      state name convention cannot be modified.
+  */
 
-  switchNameHandler = (newName) => {
+  state = {
+    people: [
+      { name: "Alex", age: 29 },
+      { name: "Liz", age: 20 },
+      { name: "Stephanie", age: 30 }
+    ]
+  };
+
+  switchNameHandler = newName => {
     //console.log("This was clicked!");
     // avoid using the following syntax for changing a state -> this.state.people[0].name = 'Alejandro'
     this.setState({
-          people: [{name: newName, age: 22},
-                  {name: 'Liz', age: 24},
-                  {name: 'Stephanie', age: 32}
-                ]})
-    }
+      people: [
+        { name: newName, age: 22 },
+        { name: "Liz", age: 24 },
+        { name: "Stephanie", age: 32 }
+      ]
+    });
+  };
 
-  nameChangeHandler = (event) => {
+  nameChangeHandler = event => {
     this.setState({
-      people: [{name: 'Alejandro!!!', age: 22},
-              {name: event.target.value, age: 24},
-              {name: 'Stephanie', age: 32}
-            ]})
-    }
+      people: [
+        { name: "Alejandro Bautista Ramos", age: 29 },
+        { name: event.target.value, age: 24 },
+        { name: "Stephanie", age: 32 }
+      ]
+    });
+  };
 
   render() {
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
     };
 
-
     return (
-      //We use className to avoid confusion with the keyword class even though the className 
-      // gets converted to the class keyword.
+      /*
+          In normal HTML, we use class in the divs but in JSX we use className to avoid confusion with the 
+          keyword class even though the className gets converted to the class keyword.
+          
+          In the React return statement, everything must be contained in one root div. 
+          
+          The this keyword refers to the class App and we specified the property that we defined.
+
+          Remember that the code from below is JSX (JavaScript compiled)
+       */
 
       <div className="App">
-        <h1>React App</h1>
-        <p>This is working!</p>
-        <button  style={style}
-        onClick={() => this.switchNameHandler('Alejandro B.')}>Switch name</button>
-        
-        <Person 
-            name={this.state.people[0].name} 
-            age={this.state.people[0].age}>
+        <h1>Section 3</h1>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}>
+          <button
+            style={style}
+            onClick={() => this.switchNameHandler("Alejandro Bautista")}
+          >
+            Click on me to switch my name
+          </button>
+          <p>I am a child 1</p>
         </Person>
-        <Person 
-            name={this.state.people[1].name} 
-            age={this.state.people[1].age}
-            //The syntax from below is more efficient than the one from above.
-            click = {this.switchNameHandler.bind(this, 'Alejandro')}
-            changed = {this.nameChangeHandler}>Hobbies: Racing
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>
+          <p>I am a child 2</p>
         </Person>
-        <Person 
-            name={this.state.people[2].name} 
-            age={this.state.people[2].age}>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}>
+          <p>I am a child 3</p>
         </Person>
-
-      </div> 
+        <Person
+          //The syntax from below is more efficient than the one from above.
+          click={this.switchNameHandler.bind(this, "Alejandro")}
+          changed={this.nameChangeHandler}
+        >
+          Click here!!!
+        </Person>
+      </div>
       // html element, css, element to render
-      //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello Alex!'))
+      // The commented section describes how React renders the html elements and it's the same as the code from above.
+      // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello Alex!'))
     );
   }
 }
--- Section-3/src/Person/Person.js
+++ Section-3/src/Person/Person.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import './Person.css';
+import React from "react";
+import "./Person.css";
 
-const person = (props) => {
-    return (
-        <div className="Person">
-            <p onClick={props.click}>I am a {props.name} and I am {props.age} years old.</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed}/> 
-        </div>
-    )
+/*  props is a convention parameter, you can type in any name you want
+    props.children displays the the content that is between <Person></Person>
+    Below is a functional component
+*/
 
+const person = props => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I am a {props.name} and I am {props.age} years old.
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} />
+    </div>
+  );
 };
 
 export default person;

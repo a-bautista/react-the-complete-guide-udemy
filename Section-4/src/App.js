ab9c819b59a3817b62bfdc475e9e27a65fb71d9c
Alex BR <alex.bautista.ramos.90@gmail.com>
2019-09-03 13:07:10 -0600

    Added comments

--- Section-4/src/App.js
+++ Section-4/src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  /* 
+    This extended class contains a state object property that contains 3 other properties. 
+    People is an array that contains 3 elements, otherState is just an example property and showPeople is a 
+    property that is used to display the rest of the card elements when a button is clicked. 
+
+  */
+
   state = {
-      people: [{id: 'a123', name: 'Alex', age: 28},
-               {id: 'b123', name: 'Liz', age: 29},
-               {id: 'c123', name: 'Stephanie', age: 30}
-              ],
-              // showPersons is used to display the cards of the other people with the if conditional
-              showPersons: false
-          }
+    people: [
+      { id: "a123", name: "Alex", age: 28 },
+      { id: "b123", name: "Liz", age: 29 },
+      { id: "c123", name: "Stephanie", age: 30 }
+    ],
+    otherState: "some other value",
+    showPeople: false
+  };
 
   // change the name of the person in each paragraph once you type in the input field
   nameChangeHandler = (event, id) => {
@@ -22,81 +30,79 @@ class App extends Component {
     // make a copy of the index of the personIndex
     const person = {
       ...this.state.people[personIndex]
-    }
+    };
 
     // change the name of the person to the new value
     person.name = event.target.value;
 
     //make a copy of the people objects
     const copyPeople = [...this.state.people];
-    
+
     //change the value of the copied object people
     copyPeople[personIndex] = person;
 
     //update the state of the old object people to the new state
-    this.setState({people: copyPeople});
-    }
- 
+    this.setState({ people: copyPeople });
+  };
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     //const persons = this.state.people.slice(); //old feature before ES6
-    //the below is a ES6 feature that creates a new array based on an existing object (people) 
+    //the below is a ES6 feature that creates a new array based on an existing object (people)
     // and this is used to avoid unpredictable behavior of our application.
     // A reference object should not be mutated, so that's why I had to use the syntax from below
-    const persons = [...this.state.people];
+    const persons = [...this.state.people]; // spread operator
     persons.splice(personIndex, 1);
     // update the previous list of persons with the updated version of persons
-    this.setState({people: persons});
-  }
+    this.setState({ people: persons });
+  };
 
-  // this method belongs to the class App and it displays the cards of each person
   togglePersonsHandler = () => {
     // copy the variable with the original value, do not change the values through the original variable
-      const doesShow = this.state.showPersons;
-      this.setState({showPersons: !doesShow});
-  }
+    const doesShow = this.state.showPeople;
+    this.setState({ showPeople: !doesShow });
+  };
 
   render() {
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      backgroundColor: "white",
+      font: "inherit",
+      border: "3px solid blue",
+      padding: "8px",
+      cursor: "pointer"
     };
 
     let persons = null;
 
-    //if showPersons === true then display the contents of the card
-    if(this.state.showPersons){
+    //if showPeople === true then display the contents of the card
+    if (this.state.showPeople) {
       persons = (
         // persons contain the mapping of each person of the array called people
         // key is used in react to keep track of all the elements id or indexes
-          <div>
-            {this.state.people.map((person, index) => {
-              return <Person 
-                      click={() => this.deletePersonHandler(index) }
-                      name={person.name} 
-                      age={person.age} 
-                      key={person.id}
-                      changed={(event) => this.nameChangeHandler(event, person.id)}
-                    />
-            })}
-          </div>
-        );
-      }
+        <div>
+          {this.state.people.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangeHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
 
     return (
-      //We use className to avoid confusion with the keyword class even though the className 
-      // gets converted to the class keyword.
-
       <div className="App">
         <h1>React App</h1>
         <p>This is working!</p>
-        <button  style={style}
-          onClick={this.togglePersonsHandler}>Switch name</button>  
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Display the rest of the card elements
+        </button>
         {persons}
-      </div>       
+      </div>
     );
   }
 }

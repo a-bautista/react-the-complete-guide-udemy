92d60879776dd421775f830c4a47c83fb9a01c23
Alex BR <alex.bautista.ramos.90@gmail.com>
2019-09-03 20:16:34 -0600

    Added notes to the error boundaries.

--- Section-5-css-enabled/src/App.js
+++ Section-5-css-enabled/src/App.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import React, { Component } from "react";
+import classes from "./App.css";
+import Person from "./Person/Person";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
+/*
+  The error boundary is enclosed in the <App></App> and we pass the key={person.id} to the error boundary, so
+  it gets inherited to the next component.   
+*/
 
 class App extends Component {
   state = {
     persons: [
-      { id: 'asfa1', name: 'Max', age: 28 },
-      { id: 'vasdf1', name: 'Manu', age: 29 },
-      { id: 'asdf11', name: 'Stephanie', age: 26 }
+      { id: "asfa1", name: "Max", age: 28 },
+      { id: "vasdf1", name: "Manu", age: 29 },
+      { id: "asdf11", name: "Stephanie", age: 26 }
     ],
-    otherState: 'some other value',
+    otherState: "some other value",
     showPersons: false
-  }
+  };
 
-  nameChangedHandler = ( event, id ) => {
-    const personIndex = this.state.persons.findIndex( p => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
-    } );
+    });
 
     const person = {
       ...this.state.persons[personIndex]
@@ -31,38 +35,40 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( { persons: persons } );
-  }
+    this.setState({ persons: persons });
+  };
 
-  deletePersonHandler = ( personIndex ) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
-    persons.splice( personIndex, 1 );
-    this.setState( { persons: persons } );
-  }
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( { showPersons: !doesShow } );
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
-  render () {
+  render() {
     let persons = null;
-    let btnClass = '';
+    let btnClass = "";
 
-    if ( this.state.showPersons ) {
+    if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map( ( person, index ) => {
-            return <ErrorBoundary   key={person.id}>
-            <Person
-              click={() => this.deletePersonHandler( index )}
-              name={person.name}
-              age={person.age}
-              changed={( event ) => this.nameChangedHandler( event, person.id )} 
-            />
-            </ErrorBoundary>
-          } )}
+          {this.state.persons.map((person, index) => {
+            return (
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={event => this.nameChangedHandler(event, person.id)}
+                />
+              </ErrorBoundary>
+            );
+          })}
         </div>
       );
 
@@ -70,22 +76,22 @@ class App extends Component {
     }
 
     const assignedClasses = [];
-    if ( this.state.persons.length <= 2 ) {
-      assignedClasses.push( classes.red ); // classes = ['red']
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red); // classes = ['red']
     }
-    if ( this.state.persons.length <= 1 ) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-        <div className={classes.App}>
-          <h1>Hi, I'm a React App</h1>
-          <p className={assignedClasses.join( ' ' )}>This is really working!</p>
-          <button
-            className={btnClass}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </div>
+      <div className={classes.App}>
+        <h1>Hi, I'm a React App</h1>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
--- Section-5-css-enabled/src/ErrorBoundary/ErrorBoundary.js
+++ Section-5-css-enabled/src/ErrorBoundary/ErrorBoundary.js
@@ -1,25 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+
+/*
+    Error boundary is a component that handles and displays custom errors in Production. The error boundary 
+    functionality isn't showed in development. 
+*/
 
 class ErrorBoundary extends Component {
-    state = {
-        hasError: false,
-        errorMessage: ''
-    }
+  state = {
+    hasError: false,
+    errorMessage: ""
+  };
 
-    componentDidCatch = (error, info) => {
-        this.setState({hasError: true, errorMessage: error});
-    }
+  componentDidCatch = (error, info) => {
+    this.setState({ hasError: true, errorMessage: error });
+  };
 
-    render (){
-        if (this.state.hasError){
-            return 
-            <div>
-                <p>Something went wrong</p>
-            </div>;
-        }else{
-            return this.props.children;
-        }            
+  render() {
+    if (this.state.hasError) {
+      return <h1>{this.state.errorMessage}</h1>;
+    } else {
+      return this.props.children;
     }
+  }
 }
 
 export default ErrorBoundary;
